78c9ef9b7695957a51c048d67c2ba878331a168b
fer967 <gabrielfcorrea3@gmail.com>
2024-06-18 18:29:34 -0300

    incluir formulario

--- src/components/Checkout/Checkout.jsx
+++ src/components/Checkout/Checkout.jsx
@@ -1,78 +1,72 @@
-import { useContext, useState } from 'react';
 import './Checkout.css';
-import { Timestamp, addDoc, collection, doc, documentId, getDocs, query, where, writeBatch } from 'firebase/firestore';
-import { CartContext } from '../../context/CartContext';
+import { useContext, useState } from "react";
+import { CartContext } from "../../context/CartContext";
+import { useForm } from 'react-hook-form';
+import { collection,addDoc } from 'firebase/firestore';
 import {db} from '../../firebase/firebase';
-import CheckoutForm from '../CheckoutForm/CheckoutForm';
 
 export default function Checkout() {
-    const [loading, setLoading] = useState(false);
-    const [orderId, setOrderId] = useState('');
+    const[pedidoId, setPedidoId] = useState('');
     const { cart, total, clearCart } = useContext(CartContext);
-    const createOrder = async ({ name, phone, email }) => {
-        setLoading(true)
 
-        try{
-            const objOrder = {
-                buyer: { name, phone, email },
-                items: cart,
-                total: total,
-                date: Timestamp.fromDate(newDate())
-            }
-            const batch = writeBatch(db)
-            const outOfStock = []
-            const ids = cart.map(prod => prod.id)
-            const productsRef = collection(db, 'products')
-            const productsAddedFromFirestore = await getDocs(query(productsRef, where(documentId(), 'in', ids)))
-            const { docs } = productsAddedFromFirestore
+    const { register, handleSubmit } = useForm();
 
-            docs.forEach(doc => {
-                const dataDoc = doc.data()
-                const stockDb = dataDoc.stock
-                const productAddedToCart = cart.find(prod => prod.id === doc.id)
-                const prodCont = productAddedToCart?.cont
-                if (stockDb >= prodCont) {
-                    batch.update(doc.ref, { stock: stockDb - prodCont })
-                } else {
-                    outOfStock.push({ id: doc.id, ...dataDoc })
-                }
-            })
-
-            if (stockDb >= prodCont) {
-                batch.update(doc.ref, { stock: stockDb - prodCont })
-            } else {
-                outOfStock.push({ id: doc.id, ...dataDoc })
-            }
-
-            if (outOfStock.length === 0) {
-                await batch.commit()
-                const orderRef = collection(db, 'orders')
-                const orderAdd = await addDoc(orderRef, objOrder)
-                setOrderId(orderAdded.id)
-                clearCart()
-            } else {
-                console.error('hay productos fuera de stock')
-            }
-
-        } catch (error) {
-            console.log(error)
-        } finally {
-            setLoading(false)
-        } 
+    const comprar = (data) => {
+        const pedido = {
+            buyer: data,
+            items: cart,
+            total: total
+        }
+        console.log(pedido);
+        const ordersRef = collection(db,'orders');
+        addDoc(ordersRef,pedido)
+        .then((doc)=>{
+            setPedidoId(doc.id);
+            clearCart();
+        })
     }
-
-    if (loading) {
-        return
-        <h1>se esta generando la orden</h1>
-    } if (orderId) {
-        return
-        <h1>el id de su orden es : {orderId}</h1>
+    if(pedidoId){
+        return(
+            <div>
+                <h1>muchas gracias por tu compra !!!</h1>
+                <p>tu numero de pedido es : {pedidoId}</p>
+            </div>
+        )
     }
 
     return (
-        <div className='checkout'>
-            <h1>checkout</h1>
-            <CheckoutForm onConfirm={createOrder} />
+        <div>
+            <form onSubmit={handleSubmit(comprar)}>
+                <input type="text" placeholder="nombre" {...register("nombre")} />
+                <input type="email" placeholder="email" {...register("email")} />
+                <input type="text" placeholder="phone" {...register("telefono")} />
+                <button type="submit">comprar</button>
+            </form>
         </div>
-    )
+    );
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
